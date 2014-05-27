8a1438f57ba098ef7ae559db0992e8156a5ab99e
Jon Tungland <jon@tungland.org>
2014-05-27 10:06:49 +0200

    missing files

---
+++ BallGameWindow/Game1.cs
@@ -0,0 +1,120 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using BallGameWindow.Objects;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+#endregion
+
+namespace BallGameWindow
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+        public List<MovableObject> Objects;
+
+        public Dictionary<string, SpriteFont> Fonts;
+
+        public Game1()
+            : base()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+
+            Fonts = new Dictionary<string, SpriteFont>();
+            Objects = new List<MovableObject>();
+            var Ball1 = new MovableObject(Color.Aqua, new Rectangle(60, 60, 10, 10), this);
+
+            Objects.Add(Ball1);
+            Components.Add(Ball1);
+            
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            Fonts["DefaultFont"] = Content.Load<SpriteFont>("DefaultFont");
+
+            //foreach (var movableObject in Objects)
+            //{
+            //    //movableObject.LoadContent();
+            //}
+            
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+
+            
+
+            // TODO: Add your update logic here
+            foreach (var movableObject in Objects)
+            {
+                movableObject.Update(gameTime);
+            }
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.White);
+
+            foreach (var movableObject in Objects)
+            {
+                movableObject.Draw(gameTime);
+            }
+
+
+            spriteBatch.Begin();
+            spriteBatch.DrawString(Fonts["DefaultFont"], "aw yee", new Vector2(32, 32), Color.Black);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ BallGameWindow/Objects/MovableObject.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Xna.Framework.Input;
+
+namespace BallGameWindow.Objects
+{
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Graphics;
+
+    public class MovableObject : DrawableGameComponent
+    {
+
+        public Rectangle Position { get; set; }
+        public Color Color { get; set; }
+        public Texture2D Texture { get; set; }
+        public new Game1 Game { get; set; }
+        public SpriteBatch spriteBatch { get; set; }
+
+        public MovableObject(Color _col, Rectangle _vec, Game _game)
+            : base(_game)
+        {
+            Game = (Game1)_game;
+            Color = _col;
+            Position = _vec;
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            Texture = new Texture2D(GraphicsDevice, 1, 1, false, SurfaceFormat.Color);
+            Texture.SetData(new[] {Color});
+
+            base.LoadContent();
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            spriteBatch.Begin();
+            spriteBatch.Draw(Texture, Position, Color);
+            spriteBatch.DrawString(Game.Fonts["DefaultFont"], Position.X + " x " + Position.Y, new Vector2(200, 200), Color.Black);
+            spriteBatch.End();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.D))
+            {
+                Position.Offset(1, 0);
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.A))
+            {
+                Position.Offset(-1, 0);
+            }
+
+        }
+    }
+}
